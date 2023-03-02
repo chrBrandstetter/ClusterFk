1dfd5d6020a0683a8d65cefdcd43d6a2337f0988
Christoph Brandstetter <christoph.br4ndstetter@gmail.com>
2023-03-02 21:39:34 +0100

    First Popup (Starting to learn WPF)

--- Just4FunApp/PopupTest.xaml.cs
+++ Just4FunApp/PopupTest.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Just4FunApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PopupTest.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PopupTest : Window
     {
-        public MainWindow()
+        public PopupTest()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            PopupTest popup = new PopupTest();
-            popup.ShowDialog();
         }
     }
 }
